600c5403eb37adbdea6eb75f1bce6f2b066b186b
O2 <o2@Mac-mini-O2.local>
2021-11-01 12:08:50 +0300

    correct promo slider arrow in desktop ver

--- src/blocks/promo-slider/promo-slider.js
+++ src/blocks/promo-slider/promo-slider.js
@@ -10,10 +10,26 @@ o2.promoSlider =
 			autoplay: true,
 			autoplaySpeed: 2000,
 			arrows: false,
+			mobileFirst: true,
+			prevArrow: '<button class="promo-slider__arrow-prev"></button>',
+			nextArrow: '<button class="promo-slider__arrow-next"></button>',
 			customPaging: function (slider, item)
 			{
 				return '<div class="promo-slider__slider-dot"></div>';
-			}
+			},
+			responsive: [
+			{
+				breakpoint: 769,
+				settings:
+				{
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					infinite: true,
+					dots: false,
+					arrows: true,
+				}
+			},
+			]
 		});
 	},
 }
