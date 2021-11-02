d3cd991ba40539422253713820d436228459024d
Amiran <chicobes1@gmail.com>
2021-11-02 20:57:11 +0300

    add slider dots in banners block mob ver

--- src/blocks/banners/banners.js
+++ src/blocks/banners/banners.js
@@ -15,19 +15,19 @@ o2.banners =
 			nextArrow: '<button class="promo-slider__arrow-next"></button>',
 			customPaging: function (slider, item)
 			{
-				return '<div class="promo-slider__slider-dot"></div>';
+				return '<div class="banners__slider-dot"></div>';
 			},
 			responsive: [
-			{
-				breakpoint: 769,
-				settings:
 				{
-					slidesToShow: 2,
-					slidesToScroll: 1,
-					dots: false,
-					arrows: false,
-				}
-			},
+					breakpoint: 769,
+					settings:
+					{
+						slidesToShow: 2,
+						slidesToScroll: 1,
+						dots: false,
+						arrows: false,
+					}
+				},
 			]
 		});
 	},
