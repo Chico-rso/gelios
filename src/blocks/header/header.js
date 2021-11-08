b78818f48c04a7e022a5da1d5419b4c3230203ec
O2 <o2@Mac-mini-O2.local>
2021-11-08 16:22:49 +0300

    add popup-user in header block

--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -3,7 +3,10 @@ o2.header =
 	activeSearch()
 	{
 		const el = document.querySelector('._header__mob-input');
-		console.log(el);
 		el.classList.toggle('active');
-	}
+	},
+	openPopup(e)
+	{
+		e.target.closest('._header__top').classList.toggle('active');
+	},
 };
