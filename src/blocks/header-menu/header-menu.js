c0605739d7ac263540eb051e4339ee71542d2218
O2 <o2@Mac-mini-O2.local>
2021-11-09 16:37:50 +0300

    add mob menu toggle

--- src/blocks/header-menu/header-menu.js
+++ src/blocks/header-menu/header-menu.js
@@ -3,5 +3,13 @@ o2.headerMenu =
 	openMenu()
 	{
 		document.querySelector('._header-menu').classList.toggle('open');
+	},
+	openCatalog()
+	{
+		document.querySelector('._header-menu__catalog').classList.toggle('open');
+	},
+	openCatalogInner()
+	{
+		document.querySelector('._header-menu__catalog-inner').classList.toggle('active');
 	}
 }
