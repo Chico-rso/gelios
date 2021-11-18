71c7bb3b78b3cac46f4c84ac680fcb4a8f6d9be4
O2 <o2@Mac-mini-O2.local>
2021-11-18 16:50:00 +0300

    correct perfect pixel in catalog

--- src/pages/catalog/catalog.js
+++ src/pages/catalog/catalog.js
@@ -2,9 +2,7 @@ o2.catalog =
 {
 	openCategory()
 	{
-		const sidebarCategory = document.querySelector('._sidebar__category');
-		const sidebarCatalog = document.querySelector('._sidebar__catalog');
-		sidebarCatalog.classList.add('active');
-		sidebarCategory.classList.add('active');
-	}
+		const catalogCategory = document.querySelector('._catalog__category').classList.add('open');
+		const catalog = document.querySelector('._catalog').classList.add('close');
+	},
 }
