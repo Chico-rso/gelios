0a4ca9789c5a75a66a811b886edef2bce5711d5a
O2 <o2@Mac-mini-O2.local>
2021-11-22 17:48:21 +0300

    create detail card file

--- src/pages/catalog/catalog.js
+++
@@ -1,8 +0,0 @@
-o2.catalog =
-{
-	openCategory()
-	{
-		const catalogCategory = document.querySelector('._catalog__category').classList.add('open');
-		const catalog = document.querySelector('._catalog').classList.add('close');
-	},
-}
