699c903334a8558ec22e28831528e19ac87fdc9b
O2 <o2@Mac-mini-O2.local>
2021-11-29 15:25:39 +0300

    complete detail card

--- src/pages/detail-card/datail-card.js
+++ src/pages/detail-card/datail-card.js
@@ -2,30 +2,30 @@ o2.detailCard =
 {
 	initSlider()
 	{
-		// $('._detail-card__slider-mob').slick({
-		// 	infinite: true,
-		// 	slidesToShow: 1,
-		// 	slidesToScroll: 1,
-		// 	// autoplay: true,
-		// 	autoplaySpeed: 2000,
-		// 	arrows: false,
-		// 	dots: true,
-		// 	mobileFirst: true,
-		// 	variableWidth: true,
-		// 	responsive: [
-		// 		{
-		// 			breakpoint: 769,
-		// 			settings:
-		// 			{
-		// 				slidesToShow: 2,
-		// 				slidesToScroll: 1,
-		// 				infinite: true,
-		// 				dots: false,
-		// 				arrows: false,
-		// 			}
-		// 		},
-		// 	],
-		// });
+		$('._detail-card__slider-mob').slick({
+			infinite: true,
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			// autoplay: true,
+			autoplaySpeed: 2000,
+			arrows: false,
+			dots: true,
+			mobileFirst: true,
+			variableWidth: true,
+			responsive: [
+				{
+					breakpoint: 769,
+					settings:
+					{
+						slidesToShow: 2,
+						slidesToScroll: 1,
+						infinite: true,
+						dots: false,
+						arrows: false,
+					}
+				},
+			],
+		});
 		$('._detail-card__slider-desktop').slick({
 			slidesToShow: 1,
 			slidesToScroll: 1,
