843d5e4288607512834f5ea6c410c073a6b2d770
Amiran <o2@Mac-mini-O2.local>
2021-12-10 17:50:06 +0300

    add close icon in header search

--- src/blocks/g-yamap/g-yamap.js
+++ src/blocks/g-yamap/g-yamap.js
@@ -6,16 +6,19 @@ o2.gYamap =
 			coords: [55.756817, 37.632305],
 			title: 'Аптека Гелиос',
 			address: 'Лубянский пр. 15 стр 2',
+			all: 'Круглосуточно',
 		},
 		{
 			coords: [55.765685, 37.599101],
 			title: 'Аптека Гелиос',
 			address: 'Новорязанская улица, 18с14',
+			all: 'Круглосуточно',
 		},
 		{
 			coords: [55.770741, 37.660577],
 			title: 'Аптека Гелиос',
 			address: 'Новорязанская улица, 18с14',
+			all: 'Круглосуточно',
 		},
 	],
 	init()
@@ -26,21 +29,23 @@ o2.gYamap =
 	{
 		let myMap = new ymaps.Map('yamap', {
 			center: [55.753220, 37.622513], // Moscow
-			zoom: 12
+			zoom: 12,
+			controls: []
 		})
 
-		o2.gYamap.data.forEach( (el) => myMap.geoObjects.add(o2.gYamap.createPlacemark(el.coords, el.title, el.address)))
+		o2.gYamap.data.forEach( (el) => myMap.geoObjects.add(o2.gYamap.createPlacemark(el.coords, el.title, el.address, el.all)))
 	},
-	createPlacemark(coords, title, address)
+	createPlacemark(coords, title, address,all)
 	{
 		return new ymaps.Placemark(coords, {
 			balloonContentHeader: title,
 			balloonContent: address,
+			balloonContentFooter: all,
 		},
 		{
 			iconLayout: 'default#image',
             iconImageHref: './svg/yamap-pointer-1.svg',
-            iconImageSize: [28, 28],
+            iconImageSize: [28, 28]
 		});
 	},
 }
--- src/blocks/header/header.js
+++ src/blocks/header/header.js
@@ -16,4 +16,23 @@ o2.header =
 		_this.querySelector('.header__svg-arrow').classList.toggle('active');
 		_this.querySelector('.header__svg-x').classList.toggle('active');
 	},
+	changeIconSearch(instance)
+	{
+		if(instance.value !== '')
+		{
+			document.querySelector('._g-input-icon-close').classList.add('active');
+			document.querySelector('._g-input-icon').classList.add('hide');
+		}
+		else
+		{
+			document.querySelector('._g-input-icon-close').classList.remove('active');
+			document.querySelector('._g-input-icon').classList.remove('hide');
+		}
+	},
+	clearInput(_this)
+	{
+		document.querySelector('._g-input__input').value = '';
+		_this.classList.remove('active');
+		document.querySelector('._g-input-icon').classList.remove('hide');
+	}
 };
